3280fed25870283224163079bb0341dbed277b59
Alif Munim <alif.munim@ryerson.ca>
2021-01-01 22:21:25 -0500

    Post to elastic from Kafka topic

--- kafkaelastic/src/main/java/com/github/alifmunim/kafka/tutorial3/ElasticSearchConsumer.java
+++ kafkaelastic/src/main/java/com/github/alifmunim/kafka/tutorial3/ElasticSearchConsumer.java
@@ -6,6 +6,11 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -13,10 +18,14 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
 
 public class ElasticSearchConsumer {
 
@@ -45,22 +54,84 @@ public class ElasticSearchConsumer {
         return client;
     }
 
+    public static KafkaConsumer<String, String> createConsumer(String topic) {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-demo-elasticsearch";
+
+        // Create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+
+    public static String extractJsonInfo(String jsonString) {
+        JSONObject obj = new JSONObject(jsonString);
+        String text = obj.getString("text");
+        String user = obj.getJSONObject("user").getString("screen_name");
+        String date = obj.getString("created_at");
+
+        String newJsonString = new JSONObject()
+                .put("text", text)
+                .put("user", user)
+                .put("date", date)
+                .toString();
+
+        return newJsonString;
+    }
+
     public static void main(String[] args) throws IOException {
         Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
 
         RestHighLevelClient client = createClient();
 
-        String jsonString = "{ \"name\": \"john\" }";
+        // Create consumer
+        KafkaConsumer<String, String> consumer = createConsumer("twitter_tweets");
 
-        IndexRequest indexRequest = new IndexRequest(
-                "twitter"
-        ).source(jsonString, XContentType.JSON);
+        int numTweets = 0;
+        boolean tweetMax = false;
+        // Poll for new data
+        while (!tweetMax) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexResponse.getId();
-        logger.info(id);
+            // Insert data into elasticsearch
+            for (ConsumerRecord<String, String> record : records) {
+                // Create a JSON string
+                String jsonString = record.value();
+                String jsonExtract = extractJsonInfo(jsonString);
 
-        client.close();
+                // Create an index request
+                IndexRequest indexRequest = new IndexRequest(
+                        "twitter02"
+                ).source(jsonExtract, XContentType.JSON);
+
+                // Send index request and get ID from response
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                String id = indexResponse.getId();
+                logger.info(id);
+
+                try {
+                    Thread.sleep(1000);
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                numTweets += 1;
+
+                if (numTweets >= 100) {
+                    tweetMax = true;
+                }
+            }
+        }
+
+//        client.close();
     }
 
 }
